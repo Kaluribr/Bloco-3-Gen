aa3598799f1ab07f0cd3e8d7be9f48ffda9622e3
Danilo Moraes <danilot.moraes@gmail.com>
2023-04-08 21:25:16 -0300

    Implementacao js

--- Fundamentos WEB/assets/js/script.js
+++ Fundamentos WEB/assets/js/script.js
@@ -14,6 +14,71 @@
     let nome = window.document.getElementById('nome');
     let email= document.querySelector('#email')
     let assunto= document.querySelector('#assunto')
+    let nomeOk = false
+    let emailOk = false
+    let assuntoOk = false
+    let mapa = document.querySelector('#mapa')
 
     nome.style.width = '100%';
-    email.style.width = '100%'
+    email.style.width = '100%';
+
+    function validaNome(){
+
+        let txt = document.querySelector('#txtNome')
+        if (nome.value.length < 3){
+            txt.innerHTML = 'Nome invalido';
+            txt.style.color = 'red'
+        }else {
+            txt.innerHTML = 'Nome valido';
+            txt.style.color = 'green'
+            nomeOk = true
+        }
+
+    }
+
+    function validaEmail(){
+
+        let txt = document.querySelector('#txtEmail')
+        if (email.value.indexOf('@') ==  -1 || email.value.indexOf('.') == -1){
+            txtEmail.innerHTML = 'E-mail invalido';
+            txtEmail.style.color = 'red'
+        }else {
+            txt.innerHTML = 'E-mail valido';
+            txt.style.color = 'green'
+            emailOk = true
+        }
+
+    }
+
+    function validaAssunto(){
+
+        let txtAssinto = document.querySelector('#assunto')
+        if (assunto.value.length >= 100){
+            txtAssunto.innerHTML = 'Texto e muito grande, digite no maximo 100 caracteres';
+            txtAssunto.style.color = 'red'
+            txtAssunto.style.display = 'block'
+        }else {
+            txtAssunto.style.display = 'none'
+            assuntoOk = true
+        }
+
+    }
+
+    function enviar(){
+        if(nomeOk == true && emailOk == true && assuntoOk == true){
+            alert('Formulario enviado com sucesso!')
+        }else{
+            alert('Preencha a formulario corretamente antes de enviar...')
+        }
+    }
+
+    function mapaZoom(){
+        mapa.style.width = '600px'
+        mapa.style.height = '600px'
+    }
+
+    
+    function mapaNormal(){
+        mapa.style.width = '300px'
+        mapa.style.height = '300px'
+    }
